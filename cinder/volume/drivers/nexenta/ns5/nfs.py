274e0a40b0c2889785c32052707d532822708bfa
Alex Deiter <alex.deiter@gmail.com>
2019-08-01 19:48:45 +0000

    None

--- cinder/volume/drivers/nexenta/ns5/nfs.py
+++ cinder/volume/drivers/nexenta/ns5/nfs.py
@@ -1692,6 +1692,7 @@ class NexentaNfsDriver(nfs.NfsDriver):
         if volume['host'] == new_volume['host']:
             #name_id = volume._name_id or volume.id
             name_id = None
+            return None
             return {'_name_id': name_id, 'provider_location': provider_location}
             payload = {'newPath': backup_volume_path}
             try:
