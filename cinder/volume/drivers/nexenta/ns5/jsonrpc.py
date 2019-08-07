0a84dbd4541de5e54eeba6cf32105eba481e4227
Alex Deiter <alex.deiter@gmail.com>
2019-08-07 14:05:50 +0000

    o

--- cinder/volume/drivers/nexenta/ns5/jsonrpc.py
+++ cinder/volume/drivers/nexenta/ns5/jsonrpc.py
@@ -565,7 +565,7 @@ class NefFilesystems(NefVolumes, NefVolumeGroups, NefDatasets, NefCollections):
                                         'for a volume.')
                 prop['enum'] = [512, 1024, 2048, 4096, 8192, 16384, 32768,
                                 65536, 131072, 262144, 524288, 1048576]
-            elif prop['api'] == self.key('thin_provisioning'):
+            elif prop['name'] == self.key('thin_provisioning'):
                 del prop['retype']
         self.properties.append({
             'name': self.key('rate_limit'),
